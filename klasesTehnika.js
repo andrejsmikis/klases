0f3a4225e5c92115661945ce62e5dacf34ef13a2
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2020-11-01 00:54:09 +0200

    ssss

--- klasesTehnika.js
+++ klasesTehnika.js
@@ -2,15 +2,7 @@ function klasesTehnika1() {
     let kabinetaNr=14;
     let datoruSkaits=0;
    
-    for(i=0;i<datoruDB.length;i++){
-        razotajs=datoruDB[i][razotajs];
-        if (razotajs="HP")
-        {
-            datoruSkaits++;
-        }
-    }
-    console.log(datoruSkaits);
-
+   
     rindas.innerHTML += `
 <tr>
 <td>${kabinetaNr}</td>
